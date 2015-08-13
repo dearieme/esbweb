2b45e5d7e3f9db80c0722242ea171fa8e79594d7
dearieme <scott@dearie.me.uk>
2015-08-13 15:21:49 +0100

    Add theme switch, active navs

--- lib/ESB/Controller/Main.pm
+++ lib/ESB/Controller/Main.pm
@@ -3,6 +3,18 @@ package ESB::Controller::Main;
 use Mojo::Base 'Mojolicious::Controller';
 
 sub theme {
+  my $c = shift;
+
+  my $alt_theme = $c->stash('theme') eq 'default' ? 'slate' : 'default';
+  my $current_page = $c->url_for;
+
+  $c->app->log->debug("Current page: [$current_page]");
+
+  $current_page =~ s!.*/!/!;
+  $current_page = '' unless $current_page =~ /html$/;
+
+  $c->stash( alt_theme_link => $alt_theme . $current_page );
+
   1;
 }
 
